dd9046f5c590836237a0787aa1680cf022f0d0b9
Ruthresh <rtrwaran5@gmail.com>
2026-07-15 19:31:42 +0530

    feat: add feature engineering preprocessing

--- src/train.py
+++ src/train.py
@@ -5,3 +5,16 @@ import numpy as np
 def credit()-> pd.DataFrame:
     credit = pd.read_csv("../data/credit_customers.csv")
     return credit
+
+def features(credit: pd.DataFrame):
+    credit['Class'] = credit['Class'].map({'good': 0 , 'bad': 1})
+    #Converts the target labels ('good', 'bad') into binary numerical values. 
+    
+    categorical_cols = credit.select_dtypes(include=["object","str"]).columns.to_list()
+    #Stores the names of all categorical (object/string) columns in python list.
+        
+    credit_dum = pd.get_dummies(credit, categorical_cols, drop_first= True)
+    #One-hot encoding categorical columns into binary features and drops
+    #The first category from each features to avoid redundant dummy vatiables.
+    
+    return credit_dum

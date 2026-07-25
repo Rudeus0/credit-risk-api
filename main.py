7f123362523ffba2ad9279b353c902690d910cf7
Ruthresh <rtrwaran5@gmail.com>
2026-07-25 18:15:49 +0530

    refactor: rename variables for clarity and consistency

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ if __name__ == "__main__":
     credit = load_data()
     
     #2. Features
-    credit_dum = features(credit)
+    num_credit = features(credit)
     
     #3. Split
-    X_train, X_test, y_train, y_test = train_split(credit_dum)
+    X_train, X_test, y_train, y_test = train_split(num_credit)
     
     #4. Build Pipline 
-    Pipeline = build_pipeline()
+    pipeline = build_pipeline()
     
     #5. Train
-    pipeline = train_model(Pipeline, X_train, y_train)
+    pipeline = train_model(pipeline, X_train, y_train)
     
     #6. Save
     save_model(pipeline)
